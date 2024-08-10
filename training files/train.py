9be0191bc8b4194e6b91bc7a5e1a6f89241b32f8
juanerick97 <70673871+juanerick97@users.noreply.github.com>
2024-08-09 21:51:42 -0400

    Model update

--- training files/train.py
+++ training files/train.py
@@ -80,11 +80,11 @@ x= df.drop(['Sport'], axis = 1)
 # Split data into test/train set
 X_train, X_test, y_train, y_test = train_test_split(x, y, test_size = 0.25, random_state = True)
 
-# Scale dataset
+"""# Scale dataset
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
-
+"""
 
 """#Random Forest
 from sklearn.ensemble import RandomForestClassifier
